e90b60e6532f963a931adc34a97d1e0a044d5f05
GabeDragg <gabriel.dragg@selu.edu>
2022-11-17 22:08:37 +0000

    Initial commit

---
+++ scripts/anova.R
@@ -0,0 +1,8 @@
+anova_crabs <- function(file.path){
+  fit_crabs <- lm(body_depth ~ sex, data = crabs)
+  aov(fit_crabs) -> anova_fit_crabs
+  sum_fit_crabs <- summary(anova_fit_crabs)
+  return(sum_fit_crabs)
+}
+
+anova_crabs("data/crabs.csv")
---
+++ scripts/jitterplot.R
@@ -0,0 +1,6 @@
+crab_plot <- function(file.path){
+  plot <- ggplot(crabs, aes(x = sex, y = body_depth, color = sex)) + geom_jitter()
+  return(plot)
+}
+
+crab_plot("data/crabs.csv")
